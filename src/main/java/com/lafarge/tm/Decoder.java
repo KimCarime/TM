da5ca35b4477116463dfb0eb195bbba92505c565
Kevin Lefevre <kevin.lefevre@in-tact.fr>
2015-05-20 17:58:48 +0200

    add: progress listener and clean up

--- src/main/java/com/lafarge/tm/Decoder.java
+++ src/main/java/com/lafarge/tm/Decoder.java
@@ -2,37 +2,37 @@ package com.lafarge.tm;
 
 import com.lafarge.tm.states.HeaderState;
 import com.lafarge.tm.states.State;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-
-import static com.lafarge.tm.utils.Convert.bytesToHex;
 
 public class Decoder {
 
     public static final int STATE_EXPIRATION_DELAY_MILLIS = 1000;
 
-    private static final Logger logger = LoggerFactory.getLogger(Decoder.class);
-
-    private MessageReceivedListener messageListener;
-    private ProgressListener progressListener;
+    private final MessageReceivedListener messageListener;
+    private final ProgressListener progressListener;
 
     private State state;
     private long lastDecode;
 
     public Decoder(MessageReceivedListener messageListener, ProgressListener progressListener) {
+        this.messageListener = messageListener;
+        this.progressListener = progressListener;
         this.state = new HeaderState(messageListener, progressListener);
         this.lastDecode = -1;
     }
 
     public void decode(byte[] in) throws IOException {
-        logger.info("will decode: {}", bytesToHex(in));
+        if (progressListener != null) {
+            progressListener.willDecode(in);
+        }
         boolean expired = this.lastDecode > 0 && (System.currentTimeMillis() - this.lastDecode > STATE_EXPIRATION_DELAY_MILLIS);
         this.lastDecode = System.currentTimeMillis();
         if (expired && !(state instanceof HeaderState)) {
-            logger.info("previous state has expired -> reset to HeaderState");
+            if (progressListener != null) {
+                progressListener.timeout();
+            }
             this.state = new HeaderState(messageListener, progressListener).decode(new ByteArrayInputStream(in));
         } else {
             this.state = this.state.decode(new ByteArrayInputStream(in));
