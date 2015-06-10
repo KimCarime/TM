f6552a1ee6c32d663c72f04860ce055af34ddf78
prolland <philippe.rolland@in-tact.fr>
2015-06-10 18:36:02 +0200

    fix: compilation test failed ...

--- TruckMixDemo/src/main/java/com/lafarge/truckmix/demo/MainActivity.java
+++ TruckMixDemo/src/main/java/com/lafarge/truckmix/demo/MainActivity.java
@@ -1,10 +1,9 @@
 package com.lafarge.truckmix.demo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.lafarge.truckmix.Communicator;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,8 +12,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Communicator com = new Communicator(null, null, null);
     }
 
     @Override
