3172c8cfc4dc33d549d686464439b95e95fe9b6c
Kevin Lefevre <kevin.lefevre@in-tact.fr>
2015-06-15 17:16:42 +0200

    add: EventParcelable

---
+++ TruckMixService/src/main/java/com/lafarge/truckmix/service/models/EventParcelable.java
@@ -0,0 +1,42 @@
+package com.lafarge.truckmix.service.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import com.lafarge.truckmix.communicator.events.Event;
+
+public class EventParcelable extends Event implements Parcelable     {
+
+    public EventParcelable(Event event) {
+        super(event.id, event.value, event.timestamp);
+    }
+
+    public EventParcelable(Parcel in) {
+        super((Event.EventId) in.readValue(Event.EventId.class.getClassLoader()), in.readInt(), in.readLong());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(value.getClass().getClassLoader());
+        dest.writeValue(id);
+        dest.writeValue(value);
+        dest.writeLong(timestamp);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Event> CREATOR = new Parcelable.Creator<Event>() {
+        @Override
+        public Event createFromParcel(Parcel in) {
+            return new EventParcelable(in);
+        }
+
+        @Override
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
+}
