d5dfa2f2c69c9da9ebdf3163ad67d7c3cede1a0a
KimCarime <kim.abdoul-carime@lafarge.com>
2015-08-27 12:24:06 +0200

    blindage MAC address
    blinde la saisie de la MAC address par l'utilisateur

--- truckmixstatic/src/main/java/com/lafarge/truckmix/tmstatic/ParametersCalculatorActivity.java
+++ truckmixstatic/src/main/java/com/lafarge/truckmix/tmstatic/ParametersCalculatorActivity.java
@@ -8,6 +8,7 @@ import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.lafarge.truckmix.tmstatic.utils.DataManager;
 
@@ -50,7 +51,12 @@ public class ParametersCalculatorActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.calculatorParam1) { //save
-            mDataManager.setMACAddrBT(mMACAddress.getText().toString());
+            if (mMACAddress.getText().toString().length()==17) {
+                mDataManager.setMACAddrBT(mMACAddress.getText().toString());
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.CalculatorParametersMACSaved), Toast.LENGTH_SHORT).show();
+            }
+            else
+                Toast.makeText(getApplicationContext(), getResources().getString(R.string.CalculatorParametersMACIncorrect), Toast.LENGTH_SHORT).show();
             return true;
         }
 
@@ -59,20 +65,115 @@ public class ParametersCalculatorActivity extends AppCompatActivity {
 
     //format the MAC address
 TextWatcher formatMAC=new TextWatcher() {
-    @Override
+        String mPreviousMac = null;
+
+        @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
     }
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
+        String enteredMac = mMACAddress.getText().toString().toUpperCase();
+        String cleanMac = clearNonMacCharacters(enteredMac);
+        String formattedMac = formatMacAddress(cleanMac);
 
+        int selectionStart = mMACAddress.getSelectionStart();
+        formattedMac = handleColonDeletion(enteredMac, formattedMac, selectionStart);
+        int lengthDiff = formattedMac.length() - enteredMac.length();
+
+        setMacEdit(cleanMac, formattedMac, selectionStart, lengthDiff);
     }
 
     @Override
     public void afterTextChanged(Editable s) {
 
     }
+        /**
+         * Strips all characters from a string except A-F and 0-9.
+         * @param mac       User input string.
+         * @return          String containing MAC-allowed characters.
+         */
+        private String clearNonMacCharacters(String mac) {
+            return mac.toString().replaceAll("[^A-Fa-f0-9]", "");
+        }
+        /**
+         * Adds a colon character to an unformatted MAC address after
+         * every second character (strips full MAC trailing colon)
+         * @param cleanMac      Unformatted MAC address.
+         * @return              Properly formatted MAC address.
+         */
+        private String formatMacAddress(String cleanMac) {
+            int grouppedCharacters = 0;
+            String formattedMac = "";
+
+            for (int i = 0; i < cleanMac.length(); ++i) {
+                formattedMac += cleanMac.charAt(i);
+                ++grouppedCharacters;
+
+                if (grouppedCharacters == 2) {
+                    formattedMac += ":";
+                    grouppedCharacters = 0;
+                }
+            }
+
+            // Removes trailing colon for complete MAC address
+            if (cleanMac.length() == 12)
+                formattedMac = formattedMac.substring(0, formattedMac.length() - 1);
+
+            return formattedMac;
+        }
+        /**
+         * Upon users colon deletion, deletes MAC character preceding deleted colon as well.
+         * @param enteredMac            User input MAC.
+         * @param formattedMac          Formatted MAC address.
+         * @param selectionStart        MAC EditText field cursor position.
+         * @return                      Formatted MAC address.
+         */
+        private String handleColonDeletion(String enteredMac, String formattedMac, int selectionStart) {
+            if (mPreviousMac != null && mPreviousMac.length() > 1) {
+                int previousColonCount = colonCount(mPreviousMac);
+                int currentColonCount = colonCount(enteredMac);
+
+                if (currentColonCount < previousColonCount) {
+                    formattedMac = formattedMac.substring(0, selectionStart - 1) + formattedMac.substring(selectionStart);
+                    String cleanMac = clearNonMacCharacters(formattedMac);
+                    formattedMac = formatMacAddress(cleanMac);
+                }
+            }
+            return formattedMac;
+        }
+        /**
+         * Gets MAC address current colon count.
+         * @param formattedMac      Formatted MAC address.
+         * @return                  Current number of colons in MAC address.
+         */
+        private int colonCount(String formattedMac) {
+            return formattedMac.replaceAll("[^:]", "").length();
+        }
+
+        /**
+         * Removes TextChange listener, sets MAC EditText field value,
+         * sets new cursor position and re-initiates the listener.
+         * @param cleanMac          Clean MAC address.
+         * @param formattedMac      Formatted MAC address.
+         * @param selectionStart    MAC EditText field cursor position.
+         * @param lengthDiff        Formatted/Entered MAC number of characters difference.
+         */
+        private void setMacEdit(String cleanMac, String formattedMac, int selectionStart, int lengthDiff) {
+            mMACAddress.removeTextChangedListener(this);
+            if (cleanMac.length() <= 12) {
+                mMACAddress.setText(formattedMac);
+                mMACAddress.setSelection(selectionStart + lengthDiff);
+                mPreviousMac = formattedMac;
+            } else {
+                mMACAddress.setText(mPreviousMac);
+                mMACAddress.setSelection(mPreviousMac.length());
+            }
+            mMACAddress.addTextChangedListener(this);
+        }
+
 };
+
 
 }
